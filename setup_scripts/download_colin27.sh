69708ede6486cfe33d1183a4dbf6ad6823825da0
Noam Peled <peled.noam@gmail.com>
2022-10-05 12:47:42 -0400

    setup scripts

--- setup_scripts/download_colin27.sh
+++ setup_scripts/download_colin27.sh
@@ -1,8 +1,6 @@
 # Getting colin27
 #if operating system is mac
-mkdir subjects
-mkdir mmvt_blend
-cd mmvt_blend || { echo "Failure - No mmvt_blend folder"; exit 1; }
+
 
 if [[ "$OSTYPE" == "darwin"* ]]
 then
@@ -14,15 +12,17 @@ fi
 
 unzip -o colin27_dropbox.zip
 rm colin27_dropbox.zip
+
+mv colin27.zip ./mmvt_blend
+cd ../mmvt_blend || { echo "Failure - No mmvt_blend folder"; exit 1; }
 unzip -o colin27.zip
-
-#n_files = !unzip -l colin27.zip | grep .png | wc -l
-#!unzip -o colin27.zip | pv -l -s {n_files[0]} > /dev/null
-
 rm colin27.zip
-mv colin27_subjects.zip ../subjects/
+cd ..
+
+mv colin27_subjects.zip ./subjects
 cd ../subjects || { echo "Failure - No subjects folder"; exit 1; }
 unzip colin27_subjects.zip
 mv colin27_subjects colin27
 rm colin27_subjects.zip
-cd ../
+cd ..
+
