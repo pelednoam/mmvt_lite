48291a89953c9ab99bdc2e69c0e690b51fd8d909
Noam Peled <peled.noam@gmail.com>
2022-10-05 12:02:16 -0400

    setup scripts

--- src/setup.py
+++ src/setup.py
@@ -241,8 +241,7 @@ def install_reqs():
     reqs_fname = op.join(utils.get_parent_fol(levels=2), 'requirements.txt')
     with open(reqs_fname, 'r') as f:
         for line in f:
-            line.strip()
-            line_parts = line.split('==')
+            line_parts = line.strip().split('==')
             print('reqs line: {}'.format(line_parts))
             if len(line_parts) == 1:
                 utils.run_script('pip install {}'.format(line_parts[0]))
